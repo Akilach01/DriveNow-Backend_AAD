0dfe649337b63f12e47e84897533b33bb4836a46
Akilach01 <akilapeiris92@gmail.com>
2025-08-19 15:06:11 +0530

    first commit

---
+++ src/main/java/org/example/drivenow_carrental_aad/DrivenowCarRentalAAdApplication.java
@@ -0,0 +1,13 @@
+package org.example.drivenow_carrental_aad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivenowCarRentalAAdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivenowCarRentalAAdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/drivenow_carrental_aad/DrivenowCarRentalAAdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.drivenow_carrental_aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivenowCarRentalAAdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
