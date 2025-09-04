9ee67183bfac4a2b47bfc720445b5f43df847028
Akilach01 <akilapeiris92@gmail.com>
2025-09-05 00:57:05 +0530

    updating usercontroller

---
+++ src/main/java/org/example/drivenow_carrental_aad/controller/UserController.java
@@ -0,0 +1,48 @@
+package org.example.drivenow_carrental_aad.controller;
+
+import org.example.drivenow_carrental_aad.dto.UserDto;
+import org.example.drivenow_carrental_aad.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //admin can also create users
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto dto) {
+        return userService.createUser(dto);
+    }
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id]")
+    public UserDto updateUser(@PathVariable Long id, @RequestBody UserDto dto) {
+        return userService.updateUser(id, dto);
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
