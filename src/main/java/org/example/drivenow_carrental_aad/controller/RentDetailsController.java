d752163b5a599a86092be95e866a6e621bda359e
Akilach01 <akilapeiris92@gmail.com>
2025-09-04 21:37:51 +0530

    updating bookingcontroller

--- src/main/java/org/example/drivenow_carrental_aad/controller/RentDetailsController.java
+++ src/main/java/org/example/drivenow_carrental_aad/controller/RentDetailsController.java
@@ -1,8 +1,11 @@
 package org.example.drivenow_carrental_aad.controller;
 
+import org.example.drivenow_carrental_aad.dto.RentDetailsDto;
 import org.example.drivenow_carrental_aad.service.RentDetailService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
@@ -13,5 +16,20 @@ public class RentDetailsController {
     public RentDetailsController(RentDetailService service) {
         this.service = service;
     }
+    @PreAuthorize("hasRole('USER')")
+@PostMapping
+    public RentDetailsDto createBooking(@RequestBody RentDetailsDto rentDetails) {
+        return service.createBooking(rentDetails);
+}
 
+    @PreAuthorize("hasRole('ADMIN')")
+@GetMapping
+    public List<RentDetailsDto> getAllBookings() {
+        return service.getAllBookings();
+}
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+@GetMapping("/{id}")
+    public RentDetailsDto getBooking(@PathVariable Long id) {
+        return service.getBookingById(id);
+}
 }
